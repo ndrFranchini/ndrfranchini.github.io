baa291b46deaf6d74044a16364bf015ba15d903e
Andrea Franchini <ndr.franchini@gmail.com>
2024-11-12 13:28:19 +0100

    Website version 3

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// main.js
+// Example: Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
